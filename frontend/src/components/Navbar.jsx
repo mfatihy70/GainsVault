dbbd485e47ed5bea13d50331c1ca81df1123e386
Emre Yüksel <if23b050@wien-technikum.at>
2025-03-24 22:04:44 +0100

    Navbar Component erstellt

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,29 @@
-const Navbar = () => {
-  return <div>Navbar</div>
-}
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import CustomNavLink from './NavLink';
+import logo from '../../assets/logo.png'; // Pfad zum Logo
 
-export default Navbar
+const CustomNavbar = () => (
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar.Brand href="/">
+      <img
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="Logo"
+      />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <CustomNavLink to="/">Home</CustomNavLink>
+        <CustomNavLink to="/exercises">Exercises</CustomNavLink>
+        <CustomNavLink to="/splits">Splits</CustomNavLink>
+        <CustomNavLink to="/profile">Profile</CustomNavLink>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default CustomNavbar;
