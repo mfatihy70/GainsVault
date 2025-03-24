b0bf89faf3774bf0491ba65a26aae8c406389dd0
mfatihy70 <mfatihyildiz1453@gmail.com>
2025-03-24 12:00:19 +0100

    changed the export functions to arrow functions

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,4 @@
-import React from "react"
-
-function Navbar() {
+const Navbar = () => {
   return <div>Navbar</div>
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 
-// Define the Homepage
-export default function HomePage() {
+const HomePage = () => {
   return <div>HomePage</div>
 }
+
+export default HomePage
