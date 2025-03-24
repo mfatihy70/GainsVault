b8a08e5536e58c90c0e7f3708b78159e270daeef
ibrahim <ie@example.com>
2025-03-24 21:06:42 +0000

    add user table creation and migration scripts; define User model with updated table name

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/db.js';
 
-const User = sequelize.define('User', {
+const User = sequelize.define('users', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
